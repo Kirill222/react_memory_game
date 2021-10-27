7db2057b4998f840e398eb198c19c1604a54969d
johnconnor <zkmd2012@inbox.ru>
2021-10-27 14:40:42 +0500

    6 Flipping cards logic: conditional props, setTimeout property to show cards,  css position absolute, rotateY properties

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,6 +1,6 @@
 import './SingleCard.css'
 
-const SingleCard = ({card, handleChoice}) => {
+const SingleCard = ({card, handleChoice, flipped}) => {
 
     const handleClick = () => {
         handleChoice(card)
@@ -9,7 +9,7 @@ const SingleCard = ({card, handleChoice}) => {
 
     return (
         <div className="card">
-            <div>
+            <div className={flipped ? "flipped" : ""}>
               <img className="front" src={card.src} alt="card front" />
               <img className="back" src="/img/cover.png" alt="card back" onClick={handleClick}/>
             </div>
