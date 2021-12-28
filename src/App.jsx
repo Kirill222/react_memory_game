615edd285064372c840349bfa9896573174b21e1
Zzz <zkmd2012@inbox.ru>
2021-12-28 14:45:26 +0100

    Changed path to images so that they work on github pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import './App.css'
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-  { "src": "/img/helmet-1.png", matched: false },
-  { "src": "/img/potion-1.png", matched: false },
-  { "src": "/img/ring-1.png", matched: false },
-  { "src": "/img/scroll-1.png", matched: false },
-  { "src": "/img/shield-1.png", matched: false },
-  { "src": "/img/sword-1.png", matched: false },
+  { "src": "./react_memory_game/img/helmet-1.png", matched: false },
+  { "src": "./react_memory_game/img/potion-1.png", matched: false },
+  { "src": "./react_memory_game/img/ring-1.png", matched: false },
+  { "src": "./react_memory_game/img/scroll-1.png", matched: false },
+  { "src": "./react_memory_game/img/shield-1.png", matched: false },
+  { "src": "./react_memory_game/img/sword-1.png", matched: false },
 ]
 
 function App() {
